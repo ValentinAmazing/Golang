d18504955ca5657134feabd26fb11120789f9666
Valentin <valentin.job1@yandex.ru>
2024-10-03 02:31:46 +0700

    Replace spaces, text toLower for inStr

--- basicTask.go
+++ basicTask.go
@@ -1,7 +1,11 @@
 package basicTask
 
+import "strings"
+
 func IsPalindrome(inStr string) bool {
 
+	inStr = strings.ReplaceAll(inStr, " ", "")
+	inStr = strings.ToLower(inStr)
 	s := 0
 	e := len(inStr)-1
 	
